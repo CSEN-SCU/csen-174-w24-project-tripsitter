ef7e7c6230ee3eb3271984f7ead4fc2ce6e44ec2
Jordan Mosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2024-01-17 10:26:51 -0800

    Added Fluro routing and Provider

---
+++ frontend/lib/pages/home.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
+      body: Center(
+        child: Text('Home Page. Need to figure out what would actually go here.'),
+      ),
+    );
+  }
+}
