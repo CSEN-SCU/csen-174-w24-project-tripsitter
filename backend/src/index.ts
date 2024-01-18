6d8af5713bd6bde30f21628a9fe76e839fbe6d4e
Jordan Mosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2024-01-18 12:04:12 -0800

    Added express and installation instructions

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,24 @@
 import * as functions from "firebase-functions";
 
+import * as express from 'express'
+const app = express();
+const cors = require('cors')({origin: true});
+app.use(cors);
+const port = 3000;
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+app.get('/test', (req, res) => {
+  res.send('endpoint2!');
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
+
 // // Start writing functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+exports.api = functions.https.onRequest(app);
