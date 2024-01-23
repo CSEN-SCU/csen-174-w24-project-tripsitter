e7f6d142a147e407f07989d17a426ea58fd47693
Cameron Greene <cam.greene@gmail.com>
2024-01-23 10:40:36 -0800

    asterisk line

--- activity1.py
+++ activity1.py
@@ -1,5 +1,6 @@
-# Add 'print("Firstname Lastname")' on a new line 
+# Add 'print("Firstname Lastname")' on a new line
 print("Jordan Mosakowski");
 camstring = "eneerG noremaC"
 print(camstring[::-1])
 
+print("* * * * * *" * 5)
