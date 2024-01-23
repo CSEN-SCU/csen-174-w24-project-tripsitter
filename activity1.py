7f47eb021c0c3ec162f317222159beed4e3372d3
darylho03 <135100043+darylho03@users.noreply.github.com>
2024-01-23 10:43:31 -0800

    Update activity1.py
    Added my name

--- activity1.py
+++ activity1.py
@@ -1,5 +1,8 @@
 # Add 'print("Firstname Lastname")' on a new line 
 print("Jordan Mosakowski");
+
+name = ["D","a","r","y","l"," ","H","o"]
+print("".join(name))
 camstring = "eneerG noremaC"
 print(camstring[::-1])
 
