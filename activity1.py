c29f0950f7dbcfe8e27d413c036ef2c52cc50271
Cameron Greene <cam.greene@gmail.com>
2024-01-23 10:50:24 -0800

    NO SEMICOLONS

--- activity1.py
+++ activity1.py
@@ -1,10 +1,10 @@
 # Add 'print("Firstname Lastname")' on a new line
-print("Jordan Mosakowski");
+print("Jordan Mosakowski")
 
 name = ["D","a","r","y","l"," ","H","o"]
 print("".join(name))
 
-print("Grant Goldman");
+print("Grant Goldman")
 print("Jacob Wang")
 
 camstring = "eneerG noremaC"
