ee58f4d42df4f69534372170cee5ec62f15a946d
Jacoob_ <43542530+Jacob5190@users.noreply.github.com>
2024-01-23 10:40:48 -0800

    Update activity1.py

--- activity1.py
+++ activity1.py
@@ -1,5 +1,6 @@
 # Add 'print("Firstname Lastname")' on a new line 
-print("Jordan Mosakowski");
+print("Jordan Mosakowski")
+print("Jacob Wang")
 camstring = "eneerG noremaC"
 print(camstring[::-1])
 
