59715c350f35dafe40f1007cd98e44a5e38d9078
Jordan Mosakowski <35510468+jordanmosakowski@users.noreply.github.com>
2024-01-26 20:00:04 -0800

    Mapbox SDK Android setup

--- frontend/lib/pages/home.dart
+++ frontend/lib/pages/home.dart
@@ -7,12 +7,12 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     const String mapboxToken = String.fromEnvironment("MAPBOX_ACCESS_TOKEN");
-    print(mapboxToken);
     return Scaffold(
       appBar: AppBar(
         title: Text('Home'),
       ),
       body: MapboxMap(
+        styleString: "mapbox://styles/mapbox/light-v11",
         accessToken: mapboxToken, 
         initialCameraPosition: CameraPosition(target: LatLng(0.0, 0.0), zoom: 1),
       )
