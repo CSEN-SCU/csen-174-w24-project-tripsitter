a5d4350cb5a55d71c544d7307f07ba0ce3417aee
Cameron Greene <cam.greene@gmail.com>
2024-01-30 15:41:12 -0800

    Navbar Componentalized

--- frontend/lib/pages/view_trip.dart
+++ frontend/lib/pages/view_trip.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hexcolor/hexcolor.dart';
 import 'package:provider/provider.dart';
+import 'package:tripsitter/components/navbar.dart';
 import 'package:tripsitter/components/trip_dash.dart';
 
 class ViewTrip extends StatelessWidget {
@@ -11,31 +12,7 @@ class ViewTrip extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(
-          'TripSitter',
-          style: GoogleFonts.kadwa(
-            textStyle: const TextStyle(
-              color: Colors.black,
-              fontWeight: FontWeight.bold,
-              fontSize: 36,
-            ),
-          ),
-        ),
-        leading: Padding(
-          padding: const EdgeInsets.all(4.0),
-          child: Image.asset('tripsitter_logo.png'),
-        ),
-        backgroundColor: HexColor("#C6D6FF"),
-        bottom: PreferredSize(
-          preferredSize: Size.fromHeight(1.0), // Set the height of the border
-          child: Container(
-            color: const Color.fromARGB(
-                255, 128, 128, 128), // Set the color of the border
-            height: 1.0, // Set the height of the border
-          ),
-        ),
-      ),
+      appBar: TripSitterNavbar(),
       body: ConstrainedBox(
         constraints: const BoxConstraints.expand(),
         child: Container(
