31b44bb9c9a47575a7fc2a2b824d34f39f933684
Cameron Greene <cam.greene@gmail.com>
2024-02-22 11:54:56 -0800

    switching to rely on FlutterMap

--- frontend/lib/components/map.dart
+++ frontend/lib/components/map.dart
@@ -1,39 +1,20 @@
 import 'package:flutter/material.dart';
-
-import 'package:mapbox_gl/mapbox_gl.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
 class TripsitterMap extends StatelessWidget {
-  const TripsitterMap({super.key});
-  late MapboxMapController mapController;
-  List<Marker> markers = [];
-
-  void _onMapCreated(MapboxMapController controller) {
-    mapController = controller;
-    // Call a function to load markers
-    loadMarkers();
-  }
-
-  void loadMarkers() {
-    // Load markers into the markers list
-    markers.add(
-      Marker(
-        markerId: MarkerId('marker1'),
-        position: LatLng(51.5, -0.09), // Marker position
-      ),
-    );
-
-    // Update the map with the new markers
-    mapController.addMarkers(markers);
-  }
+  TripsitterMap({super.key});
 
   @override
   Widget build(BuildContext context) {
     const String mapboxToken = String.fromEnvironment("MAPBOX_ACCESS_TOKEN");
-    return MapboxMap(
-      styleString: "mapbox://styles/mapbox/light-v11",
-      accessToken: mapboxToken,
-      initialCameraPosition: CameraPosition(target: LatLng(0.0, 0.0), zoom: 13),
-      markers: {},
+
+    return FlutterMap(
+      options: MapOptions(
+          initialCenter: LatLng(51.5074, 0.1278),
+          initialZoom: 16.0,
+          maxZoom: 10.0),
+      children: [TileLayer(urlTemplate: 'https://api.mapbox.com/styles/v1/drobotcamo/clsxmrjm6004901pt97s9caep/tiles/256/{z}/{x}/{y}@2x?access_token=$mapboxToken',)],
     );
   }
 }
