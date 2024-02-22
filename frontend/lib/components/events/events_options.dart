432c9afa87612337cbbea541edef038d80fe7a6b
cyin100 <conner.yin@gmail.com>
2024-02-22 00:27:18 -0800

    Alternating colors + Selected Buttons Fixed

--- frontend/lib/components/events/events_options.dart
+++ frontend/lib/components/events/events_options.dart
@@ -12,14 +12,14 @@ class EventsOptions extends StatefulWidget {
   final Trip trip;
   final List<UserProfile> profiles;
   final Function? setState;
-  const EventsOptions({required this.trip, required this.profiles, this.setState, super.key});
+  const EventsOptions(
+      {required this.trip, required this.profiles, this.setState, super.key});
 
   @override
   State<EventsOptions> createState() => _EventsOptionsState();
 }
 
 class _EventsOptionsState extends State<EventsOptions> {
-
   List<TicketmasterEvent> events = [];
 
   Trip get trip => widget.trip;
@@ -33,7 +33,8 @@ class _EventsOptionsState extends State<EventsOptions> {
 
   Future<void> getEvents() async {
     print("Getting events for trip ${trip.id}");
-    List<TicketmasterEvent> call = await TripsitterApi.getEvents(TicketmasterQuery(
+    List<TicketmasterEvent> call =
+        await TripsitterApi.getEvents(TicketmasterQuery(
       lat: trip.destination.lat,
       long: trip.destination.lon,
       startDateTime: trip.startDate,
@@ -47,81 +48,136 @@ class _EventsOptionsState extends State<EventsOptions> {
 
   @override
   Widget build(BuildContext context) {
+    // Initialize a counter variable before mapping the events to TableRows
+    int rowIndex = 0;
+
     return ListView(
       children: [
-        Text("Choose Activites", style: Theme.of(context).textTheme.displayMedium?.copyWith(decoration: TextDecoration.underline, fontWeight: FontWeight.bold)),
-        if(events.isNotEmpty)
+        Text("Choose Activities",
+            style: Theme.of(context).textTheme.displayMedium?.copyWith(
+                decoration: TextDecoration.underline,
+                fontWeight: FontWeight.bold)),
+        if (events.isNotEmpty)
           Table(
-            columnWidths: const <int, TableColumnWidth>{
-              0: FixedColumnWidth(75),
-              1: FlexColumnWidth(1),
-              2: FlexColumnWidth(1),
-              3: FixedColumnWidth(50),
-              4: FixedColumnWidth(150),
-            },
-            defaultVerticalAlignment: TableCellVerticalAlignment.middle,
-            children: events.map((event) => TableRow(
-              children: [
-                TableCell(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: event.images.isEmpty ? Icon(Icons.star) : Image.network(event.images.first.url, height: 50),
-                  )
-                ),
-                TableCell(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text(event.prices.isEmpty ? event.name :"${event.name}\nFrom \$${event.prices.map((p) => p.min).reduce(min)}/person"),
-                  )
-                ),
-                TableCell(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text('${event.venues.firstOrNull?.name}\nStarts ${event.startTime.localDate} ${event.startTime.localTime}'),
-                  )
-                ),
-                TableCell(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: IconButton(
-                      icon: Icon(Icons.info),
-                      onPressed: () {
-                        showDialog(
-                          context: context,
-                          builder: (BuildContext context) {
-                            return EventPopup(event);
-                          },
-                        );
-                      }
-                    ),
-                  )
-                ),
-                TableCell(
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Builder(
-                      builder: (context) {
-                        bool selected =  trip.activities.map((e) => e.event.id).contains(event.id);
-                        return ElevatedButton(
-                          onPressed: selected ? null : () async {
-                            await trip.addActivity(event, widget.profiles.map((e) => e.id).toList());
-                            setState(() {});
-                            if(widget.setState != null) {
-                              widget.setState!();
-                            }
-                          },
-                          style: ButtonStyle(
-                            backgroundColor: MaterialStateProperty.all<Color>(selected ? Color.fromARGB(255, 127, 166, 198) : Colors.grey[300]!)
+              columnWidths: const <int, TableColumnWidth>{
+                0: FixedColumnWidth(75),
+                1: FlexColumnWidth(1),
+                2: FlexColumnWidth(1),
+                3: FixedColumnWidth(50),
+                4: FixedColumnWidth(150),
+              },
+              defaultVerticalAlignment: TableCellVerticalAlignment.middle,
+              children: events.map((event) {
+                // Determine the background color based on the row index
+                Color bgColor = rowIndex % 2 == 0
+                    ? Colors.grey[200]! // Light gray color
+                    : Colors.white; // White color
+
+                // Increment the row index for the next iteration
+                rowIndex++;
+
+                return TableRow(
+                    decoration: BoxDecoration(
+                        color: bgColor), // Apply the background color here
+                    children: [
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: event.images.isEmpty
+                            ? Icon(Icons.star)
+                            : Image.network(event.images.first.url, height: 50),
+                      )),
+                      TableCell(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: RichText(
+                            text: TextSpan(
+                              children: [
+                                TextSpan(
+                                  text: event.name,
+                                  style: TextStyle(
+                                      fontWeight:
+                                          FontWeight.bold, // Make text bold
+                                      color: Colors
+                                          .black), // Ensure text color is consistent
+                                ),
+                                TextSpan(
+                                  text: event.prices.isEmpty
+                                      ? ''
+                                      : "\nFrom \$${event.prices.map((p) => p.min).reduce(min)}/person",
+                                  style: TextStyle(
+                                      fontWeight:
+                                          FontWeight.normal, // Regular text
+                                      color: Colors
+                                          .black), // Ensure text color is consistent
+                                ),
+                              ],
+                            ),
                           ),
-                          child: Text('Select${selected ? 'ed' : ''}', style: TextStyle(color: Colors.black)),
-                        );
-                      }
-                    ),
-                  ),
-                ),
-              ]
-            )).toList()
-          ),
+                        ),
+                      ),
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Text(
+                            '${event.venues.firstOrNull?.name}\nStarts ${event.startTime.localDate} ${event.startTime.localTime}'),
+                      )),
+                      TableCell(
+                          child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: IconButton(
+                            icon: Icon(Icons.info),
+                            onPressed: () {
+                              showDialog(
+                                context: context,
+                                builder: (BuildContext context) {
+                                  return EventPopup(event);
+                                },
+                              );
+                            }),
+                      )),
+                      TableCell(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Builder(builder: (context) {
+                            bool selected = trip.activities
+                                .map((e) => e.event.id)
+                                .contains(event.id);
+                            return ElevatedButton(
+                              onPressed: selected
+                                  ? () async {
+                                      await trip.removeActivity(trip.activities
+                                          .firstWhere(
+                                              (a) => a.event.id == event.id));
+                                      setState(() {});
+                                      if (widget.setState != null) {
+                                        widget.setState!();
+                                      }
+                                    }
+                                  : () async {
+                                      await trip.addActivity(
+                                          event,
+                                          widget.profiles
+                                              .map((e) => e.id)
+                                              .toList());
+                                      setState(() {});
+                                      if (widget.setState != null) {
+                                        widget.setState!();
+                                      }
+                                    },
+                              style: ButtonStyle(
+                                  backgroundColor:
+                                      MaterialStateProperty.all<Color>(selected
+                                          ? Color.fromARGB(255, 127, 166, 198)
+                                          : Colors.grey[300]!)),
+                              child: Text('Select${selected ? 'ed' : ''}',
+                                  style: TextStyle(color: Colors.black)),
+                            );
+                          }),
+                        ),
+                      ),
+                    ]);
+              }).toList()),
       ],
     );
   }
